b5b8d3220699ff5a07d8f4444c11b63424b35a19
Nuno Costa <91nunocosta@gmail.com>
2021-11-11 10:24:41 +0000

    tests: Add doctests execution to nox

--- noxfile.py
+++ noxfile.py
@@ -1,18 +1,15 @@
-"""nox configuration."""
-from pathlib import Path
-
+"nox configuration." ""
 import nox_poetry
 
 
 PYTHON_VERSIONS = ["3.9"]
-SRC_DIR = Path(__file__).parent.name
+SRC_DIR = "python_package"
 TEST_COVERAGE = 100
 
 
 @nox_poetry.session()
 def lint(session):
     """Lint the source code."""
-    # start_poetry(session)
     session.install("pre-commit")
     session.run("pre-commit", "run", "--all-files")
 
@@ -20,10 +17,18 @@ def lint(session):
 @nox_poetry.session()
 def tests(session):
     """Test."""
-    # start_poetry(session)
     session.install(".")
     session.install("pytest")
     session.install("pytest-cov")
     session.install("coverage-badge")
-    session.run("pytest", f"--cov-fail-under={TEST_COVERAGE}", "--cov", "tests/")
+    session.install("nox-poetry")
+
+    session.run(
+        "pytest",
+        f"--cov-fail-under={TEST_COVERAGE}",
+        f"--cov={SRC_DIR}",
+        "--doctest-modules",
+        "--doctest-glob=README.md",
+    )
+
     session.run("coverage-badge", "-f", "-o", "coverage.svg")
