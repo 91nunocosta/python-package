6df2ad6d983b87620abc4d8e975f4cee6a35cb61
Nuno Costa <91nunocosta@gmail.com>
2021-11-25 20:14:36 +0000

    chore: update noxfile

--- noxfile.py
+++ noxfile.py
@@ -1,33 +1,36 @@
-"nox configuration." ""
-import nox_poetry
+"""nox configuration."""
+from pathlib import Path
 
-PYTHON_VERSIONS = ["3.9"]
-SRC_DIR = "python_package"
+import nox
+from poetry.core.pyproject.toml import PyProjectTOML
+
+PROJECT = PyProjectTOML(Path.cwd() / "pyproject.toml").poetry_config
+
+PROJECT_NAME = PROJECT["name"]
+PROJECT_VERSION = PROJECT["version"]
+
+SRC_DIR = PROJECT_NAME.replace("-", "_")
+
 TEST_COVERAGE = 100
 
 
-@nox_poetry.session()
-def lint(session):
+@nox.session(python=None, reuse_venv=True)
+def check(session):
     """Lint the source code."""
-    session.install("pre-commit")
-    session.run("pre-commit", "run", "--all-files")
+    session.install("poetry")
+    session.run("poetry", "run", "pre-commit", "run", "--all-files", external=True)
 
 
-@nox_poetry.session()
-def tests(session):
-    """Test."""
-    session.install(".")
+@nox.session(python=None, reuse_venv=True)
+def build(session):
+    """Build the package from the source code."""
+    session.install("poetry")
+    session.run("poetry", "build", "-f", "wheel")
+
+
+@nox.session(python="3.9")
+def tests_package(session):
+    """Test package."""
+    session.install(f"./dist/{SRC_DIR}-{PROJECT_VERSION}-py3-none-any.whl")
     session.install("pytest")
-    session.install("pytest-cov")
-    session.install("coverage-badge")
-    session.install("nox-poetry")
-
-    session.run(
-        "pytest",
-        f"--cov-fail-under={TEST_COVERAGE}",
-        f"--cov={SRC_DIR}",
-        "--doctest-modules",
-        "--doctest-glob=README.md",
-    )
-
-    session.run("coverage-badge", "-f", "-o", "coverage.svg")
+    session.run("pytest", "tests/")
