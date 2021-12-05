45473f251783b7a6ff1e674c5a54ae3cdf5255ed
91nunocosta <91nunocosta@gmail.com>
2021-12-05 22:52:47 +0000

    feat: implement fib

--- prototype_python_library/__init__.py
+++ prototype_python_library/__init__.py
@@ -1,11 +1,17 @@
 """
->>> from prototype_python_library import do
->>> do()
-Doing!
+>>> from prototype_python_library import fib
+>>> fib(0)
+0
 """
 
 __version__ = "0.3.0"
 
 
-def do():
-    print("Doing!")
+def fib(n: int):
+    if n == 0:
+        return 0
+
+    if n == 1:
+        return 1
+
+    return fib(n - 1) + fib(n - 2)
--- tests/test_prototype_python_library.py
+++ tests/test_prototype_python_library.py
@@ -1,5 +1,10 @@
-from prototype_python_library import do
+from prototype_python_library import fib
 
 
-def test_do():
-    assert do() is None
+def test_fib():
+    assert fib(0) == 0
+    assert fib(1) == 1
+    assert fib(2) == 1
+    assert fib(3) == 2
+    assert fib(4) == 3
+    assert fib(5) == 5
