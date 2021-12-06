24122a9dd18e1a4d6e66079d87f7fde3ccdf3d41
91nunocosta <91nunocosta@gmail.com>
2021-12-06 19:03:08 +0000

    style: add pydocstyle

--- prototype_python_library/__init__.py
+++ prototype_python_library/__init__.py
@@ -1,4 +1,6 @@
 """
+An empty python package.
+
 >>> from prototype_python_library import fib
 >>> fib(0)
 0
@@ -8,6 +10,7 @@ __version__ = "0.4.0"
 
 
 def fib(n: int):
+    """Compute the nth element in the fibonacci sequence."""
     if n == 0:
         return 0
 
