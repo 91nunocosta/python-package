b93b5a60faa11b69ae31f9341e8ad3e1f4d86b77
91nunocosta <91nunocosta@gmail.com>
2023-02-09 18:36:43 +0000

    Update from https://github.com/91nunocosta/python-package-cookiecutter/commit/242bb7338dfdda1eda38ffd33621c6422af9cc94

--- tests/test_prototype_python_library.py
+++ tests/test_prototype_python_library.py
@@ -1,39 +1,5 @@
 """Test prototype_python_library module."""
-from prototype_python_library import (
-    Calculator,
-    add,
-    divide,
-    fib,
-    multiply,
-    power,
-    subtract,
-)
-
-
-def test_add():
-    """Test add function."""
-    assert add(0, 0) == 0
-    assert add(1, 2) == 3
-
-
-def test_subtract():
-    """Test subtract function."""
-    assert subtract(5, 3) == 2
-
-
-def test_multiply():
-    """Test multiply function."""
-    assert multiply(2, 2) == 4
-
-
-def test_power():
-    """Test power function."""
-    assert power(2, 2) == 4
-
-
-def test_division():
-    """Test divide function."""
-    assert divide(4, 2) == 2
+from prototype_python_library import fib
 
 
 def test_fib():
@@ -45,15 +11,3 @@ def test_fib():
     assert fib(4) == 3
     assert fib(5) == 5
     assert fib(6) == 8
-
-
-def test_calculator():
-    """Test calculator class."""
-    calculator = Calculator()
-    assert calculator.state == 0
-
-    calculator.add(50)
-    assert calculator.state == 50
-
-    calculator.add(100)
-    assert calculator.state == 150
